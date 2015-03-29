b731ff0f9e6ba66c7aed4d9d32cbda1e63ba7cff
Blair Anderson <blair@repairshopr.com>
2015-03-29 11:51:42 -0700

    remove https from CDN url

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ String.prototype.toCamelCase = function() {
     return p1.toLowerCase();
   });
 };
-String.prototype.toDash = function(){
-  return this.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();});
+String.prototype.toDash = function() {
+  return this.replace(/([A-Z])/g, function($1) {
+    return "-" + $1.toLowerCase();
+  });
 };
 
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var compressor = require('node-minify');
 
 var url = 'http://google.github.io/material-design-icons/';
 
@@ -24,8 +27,9 @@ var PROD = "cdn.";
 var DEV = '';
 var ENV = PROD;
 var OUTPUT_CSSFILE = "./dist/google-material-design-icons-sass.css";
+var OUTPUT_CSSMINFILE = "./dist/google-material-design-icons-sass.min.css";
 var OUTPUT_SCSSFILE = "./dist/google-material-design-icons-sass.scss";
-var HEADER_DATA = "// Base Class Definition \n \n \n"+ICON_PREFIX+":after {display: block;content: ' ';width: 24px;height: 24px;margin: 9px 5px;float: left;} \n \n \n";
+var HEADER_DATA = "\n" + ICON_PREFIX + ":after {display: block;content: ' ';width: 24px;height: 24px;margin: 9px 5px;float: left;} \n \n \n";
 
 
 fs.writeFileSync(OUTPUT_CSSFILE, '');
@@ -40,14 +44,24 @@ request(url, function(error, response, html) {
     $('div').each(function() {
       var data = $(this),
         class_name = data.text(),
-        className = ICON_PREFIX+"-"+class_name.toCamelCase().toDash(),
-        img = data.children()['0'].attribs;
-        imgURL = img.src.replace("./", "https://"+ENV+"rawgit.com/google/material-design-icons/gh-pages/"),
+        className = ICON_PREFIX + "-" + class_name.toCamelCase().toDash(),
+        img = data.children()['0'].attribs,
+        imgURL = img.src.replace("./", "//" + ENV + "rawgit.com/google/material-design-icons/gh-pages/"),
         result = className + ":after{background:url(" + imgURL + ");}\n";
 
-      fs.appendFileSync(OUTPUT_CSSFILE, result)
-      fs.appendFileSync(OUTPUT_SCSSFILE, result)
+      console.log("added: "+class_name);
+      fs.appendFileSync(OUTPUT_CSSFILE, result);
+      fs.appendFileSync(OUTPUT_SCSSFILE, result);
     });
 
+    new compressor.minify({
+      type: 'clean-css',
+      fileIn: [OUTPUT_CSSFILE],
+      fileOut: OUTPUT_CSSMINFILE,
+      callback: function(err, min) {
+        console.log('Clean-css');
+        console.log(err);
+      }
+    });
   }
 });
